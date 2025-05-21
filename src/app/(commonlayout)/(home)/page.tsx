e31483c0b6429aa830b832b367407e60b1936511
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-21 14:47:37 +0600

    added dynamic page

--- src/app/(commonlayout)/(home)/page.tsx
+++ src/app/(commonlayout)/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Banner from '@/components/modules/home/Banner';
+import Dynamic from '@/components/modules/home/Dynamic';
 import Heading from '@/components/modules/home/Heading';
 import Info from '@/components/modules/home/Info';
 import React from 'react';
@@ -7,6 +8,7 @@ const HomePage = () => {
     return (
         <div>
             <Info></Info>
+            <Dynamic></Dynamic>
             <Heading></Heading>
             <Banner></Banner>
         </div>
---
+++ src/components/modules/home/Dynamic.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dynamic = () => {
+    return (
+        <div>
+            <h2>dynamic</h2>
+        </div>
+    );
+};
+
+export default Dynamic;
