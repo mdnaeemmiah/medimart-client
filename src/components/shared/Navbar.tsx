62af55d7fe036c752f9c8c3ee0c588453947efe2
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-21 14:20:45 +0600

    added

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -340,7 +340,7 @@ export default function Navbar() {
 
 
 return (
-<div className="flex items-center justify-between px-6 py-4  shadow-md relative">
+<div className="flex items-center justify-between px-6 py-4  shadow-md bg-gray-900 fixed top-0 left-0 w-full z-50">
     <nav className="flex items-center justify-between container mx-auto">
     {/* Left Side: Logo */}
     <div className="flex items-center gap-2  ">
