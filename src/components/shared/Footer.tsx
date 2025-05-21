44e2eb955cbb188008815505dbd1480faaafb166
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-21 14:12:32 +0600

    added fotter color

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-10 px-6">
+    <footer className=" py-10 px-6 shadow-[0_10px_60px_rgba(0,0,0,0.25)]">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {/* Column 1: About Our Company */}
         <div>
