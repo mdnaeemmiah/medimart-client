800cc1a5fbb3c6e97de97c573e0328fea87c9fd3
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-23 12:00:28 +0600

    added properly

--- src/app/(commonlayout)/doctor/page.tsx
+++ src/app/(commonlayout)/doctor/page.tsx
@@ -8,7 +8,6 @@ const DoctorsPage = () => {
   const { data, isLoading, isError } = useGetDoctorsQuery(undefined);
 
   const doctors = data?.data || [];
-  console.log(doctors)
 
   if (isLoading) return <p className="text-center mt-10">Loading doctors...</p>;
   if (isError) return <p className="text-center mt-10 text-red-500">Failed to load doctors.</p>;
@@ -16,9 +15,9 @@ const DoctorsPage = () => {
   return (
     <div className="p-6">
       <h2 className="text-3xl font-bold mb-6 text-center">Our Doctors</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6">
         {doctors.map((doctor: any) => (
-          <div key={doctor._id} className=" rounded-2xl shadow-md p-4 text-center">
+          <div key={doctor._id} className="  rounded-2xl shadow-[0_4px_20px_rgba(0,0,0,0.5)] p-4 text-left">
             <Image
               src={doctor.image}
               alt={doctor.name}
