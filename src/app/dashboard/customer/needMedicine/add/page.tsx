d02aedd989de6b4dcccca1055209b5c535d2882f
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-25 02:52:58 +0600

    added

--- src/app/dashboard/customer/needMedicine/add/page.tsx
+++ src/app/dashboard/customer/needMedicine/add/page.tsx
@@ -1,6 +1,8 @@
+import { useCreateNeedMedicineMutation } from '@/redux/features/needMedicine/needMedicineSlice';
 import React from 'react';
 
 const page = () => {
+    const {}=useCreateNeedMedicineMutation();
     return (
         <div>
             <h2>add page</h2>
