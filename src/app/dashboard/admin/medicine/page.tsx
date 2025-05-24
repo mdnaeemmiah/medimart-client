7d91481707ce8be27ba1118055f4f6d54ff8d52f
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-24 13:27:39 +0600

    addded proeprly

---
+++ src/app/dashboard/admin/medicine/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h2>medicine</h2>
+        </div>
+    );
+};
+
+export default page;
--- src/components/modules/dashboard/ASidebar/app-sidebar.tsx
+++ src/components/modules/dashboard/ASidebar/app-sidebar.tsx
@@ -56,8 +56,8 @@ const data = {
       ],
     },
     {
-      title: "All-Meals",
-      url: "AllMeals",
+      title: "All-Medicines",
+      url: "medicine",
       icon: SquareTerminal,
       isActive: true,
     },
