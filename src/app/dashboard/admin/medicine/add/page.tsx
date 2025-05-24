c6fd24dbea059c3cfcca51ee20c48a5720174317
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-24 13:34:36 +0600

    added proeprly medicine add

--- src/app/dashboard/admin/medicine/add/page.tsx
+++ src/app/dashboard/admin/medicine/add/page.tsx
@@ -1,6 +1,9 @@
+import { useDeleteMedicineMutation,useGetMedicinesQuery,useUpdateMedicineMutation } from '@/redux/features/medicine/medicineSlice';
 import React from 'react';
 
 const page = () => {
+   const {} = useDeleteMedicineMutation()
+
     return (
         <div>
             <h1>add medicine</h1>
