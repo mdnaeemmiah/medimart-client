ddd17632dfac04e2ccd59be6b49533ed27c7d9b2
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-25 11:56:17 +0600

    added

--- src/components/modules/home/MedicineShop.tsx
+++ src/components/modules/home/MedicineShop.tsx
@@ -60,14 +60,14 @@ const MedicineShop = () => {
   };
 
   return (
-    <div className="p-4  shadow-lg rounded-2xl border border-gray-200 text-center">
-      <h2 className="text-xl font-semibold mb-4">Available Medicines</h2>
+    <div className="p-4  shadow-lg rounded-2xl   text-center">
+      <h2 className="text-xl text-violet-600 font-semibold mb-4">Available Medicines</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-left">
         {medicines.map((medicine) => (
           <div
             key={medicine._id}
-            className="p-4  rounded-lg  shadow-[0_4px_20px_rgba(0,0,0,0.5)]  transition-transform duration-300 hover:scale-100"
+            className="p-4 border-2 rounded-lg  shadow-[0_4px_20px_rgba(0,0,0,0.5)]  transition-transform duration-300 hover:scale-100"
           >
             <p ><strong>ID:</strong> {medicine.id}</p>
             <p ><strong>Name:</strong> {medicine.name}</p>
