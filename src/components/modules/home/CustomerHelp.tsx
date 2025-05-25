69fd93e6818cb4afa6a047f13537bab3b8ee46e7
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-25 14:15:24 +0600

    added

---
+++ src/components/modules/home/CustomerHelp.tsx
@@ -0,0 +1,13 @@
+import { useGetHelpRequestsQuery } from '@/redux/features/needHelp/needHelpSlice';
+import React from 'react';
+
+const CustomerHelp = () => {
+    const {} =  useGetHelpRequestsQuery();
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CustomerHelp;
--- src/components/modules/home/CustomerNeed.tsx
+++ src/components/modules/home/CustomerNeed.tsx
@@ -13,7 +13,7 @@ const CustomerNeed = () => {
 
   return (
     <section className="px-4 py-10 ">
-      <h2 className="text-3xl font-bold text-center mb-10 text-blue-700">
+      <h2 className="text-3xl font-bold text-center mb-10 text-violet-700">
        Customer  Requested Medicines
       </h2>
 
