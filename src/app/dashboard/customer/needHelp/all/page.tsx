e2c88a1980f57bb58382a9dfa2811aa6777633f6
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-25 13:46:39 +0600

    added

--- src/app/dashboard/customer/needHelp/all/page.tsx
+++ src/app/dashboard/customer/needHelp/all/page.tsx
@@ -1,6 +1,8 @@
+import { useGetHelpRequestsQuery,useDeleteHelpRequestMutation,useUpdateHelpRequestMutation } from '@/redux/features/needHelp/needHelpSlice';
 import React from 'react';
 
 const page = () => {
+    const {} =useGetHelpRequestsQuery()
     return (
         <div>
             <h2>all help</h2>
