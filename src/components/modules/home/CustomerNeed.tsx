6b074d581ab7f77c2d3b2c268c5c33dada683773
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-25 11:53:08 +0600

    added

--- src/components/modules/home/CustomerNeed.tsx
+++ src/components/modules/home/CustomerNeed.tsx
@@ -13,7 +13,7 @@ const CustomerNeed = () => {
   return (
     <section className="px-4 py-10 ">
       <h2 className="text-3xl font-bold text-center mb-10 text-blue-700">
-        Requested Medicines
+       Customer  Requested Medicines
       </h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
